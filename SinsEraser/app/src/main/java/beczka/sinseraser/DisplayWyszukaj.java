f2bda9d24532e0ca682ee2dabc0189d7935bb57c
MooVx <michal.tajak@gmail.com>
2017-05-06 16:08:33 +0200

    wszystkie trasy lista

--- SinsEraser/app/src/main/java/beczka/sinseraser/DisplayWyszukaj.java
+++ SinsEraser/app/src/main/java/beczka/sinseraser/DisplayWyszukaj.java
@@ -2,16 +2,23 @@ package beczka.sinseraser;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 /**
  * Created by Krasnal on 06.05.2017.
  */
 
-public class DisplayWyszukaj extends Activity
-{
-    @Override
+public class DisplayWyszukaj extends Activity {
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wyszukaj_layout);
+
     }
 }
