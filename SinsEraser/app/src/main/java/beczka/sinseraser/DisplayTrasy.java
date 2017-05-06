80c74b36083d61b7d13da7f5c6dd0f74f84e33b4
MooVx <michal.tajak@gmail.com>
2017-05-06 17:07:11 +0200

    DOBRE!

--- SinsEraser/app/src/main/java/beczka/sinseraser/DisplayTrasy.java
+++ SinsEraser/app/src/main/java/beczka/sinseraser/DisplayTrasy.java
@@ -2,6 +2,39 @@ package beczka.sinseraser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.app.Activity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.app.AlertDialog;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Krasnal on 06.05.2017.
@@ -9,9 +42,51 @@ import android.os.Bundle;
 
 public class DisplayTrasy extends Activity
 {
-    @Override
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trasy_layout);
+
+        list = (ListView) findViewById(R.id.lista_all);
+
+        String cars[] = {"Mercedes", "Fiat", "Ferrari", "Aston Martin", "Lamborghini", "Skoda", "Volkswagen", "Audi", "Citroen"};
+
+        ArrayList<String> carL = new ArrayList<String>();
+        carL.addAll(Arrays.asList(cars));
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, cars);
+
+
+        // Assign adapter to ListView
+        list.setAdapter(adapter);
+
+        list.setTextFilterEnabled(true);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                AlertDialog alertDialog = new AlertDialog.Builder(DisplayTrasy.this).create();
+                alertDialog.setTitle(((TextView) view).getText());
+                alertDialog.setMessage("tutaj informacje");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "przeglądaj dalej",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+
+
+            }
+        });
+
     }
+
+
 }
+
---
+++ SinsEraser/app/src/main/java/beczka/sinseraser/TracksBase.java
@@ -0,0 +1,59 @@
+package beczka.sinseraser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TracksBase {
+
+    public static Map<Integer,Track> tracksMap = new HashMap<Integer,Track>();
+    static String page;
+
+    public static void main(String [] args){
+        try {
+            page=Downloader.downloadPage("http://www.edk.org.pl/trasy.html");
+            page = page.replaceAll( "&([^;]+(?!(?:\\w|;)))", "&amp;$1" );
+            page = page.replaceAll("<head>.*</head>","" );
+            page = page.replaceAll("nbsp","" );
+            String page2 = Downloader.prepareXMLPage(page);
+            Downloader.parseXml(page2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for(String s: allTracks()){
+            System.out.println(s);
+        }
+
+
+    }
+
+
+
+    public void updateTracks(){
+
+    }
+
+    public static String[] allTracks(){
+        ArrayList<String> tmp = new ArrayList<String>();
+        for(Map.Entry<Integer, Track> entry : tracksMap.entrySet()){
+            tmp.add(entry.getValue().getFirstLevel());
+        }
+        return tmp.toArray(new String[tmp.size()]);
+    }
+
+
+    public static String[] proposeTracks(int distance){
+        ArrayList<String> tmp = new ArrayList<String>();
+        int dis1=(int) (distance*0.9);
+        int dis2=(int) (distance*1.1);
+        for(Map.Entry<Integer, Track> entry : tracksMap.entrySet()){
+            if(entry.getValue().getDistance()>=dis1 && entry.getValue().getDistance()<=dis2)
+                tmp.add(entry.getValue().getFirstLevel());
+        }
+
+        return tmp.toArray(new String[tmp.size()]);
+    }
+
+
+}
