7095c1149de5b06f58623e56e81c679333a67311
MooVx <michal.tajak@gmail.com>
2017-05-06 16:38:07 +0200

    Revert "wszystkie trasy lista"
    This reverts commit f2bda9d24532e0ca682ee2dabc0189d7935bb57c.

--- SinsEraser/app/src/main/java/beczka/sinseraser/DisplayWyszukaj.java
+++ SinsEraser/app/src/main/java/beczka/sinseraser/DisplayWyszukaj.java
@@ -2,23 +2,16 @@ package beczka.sinseraser;
 
 import android.app.Activity;
 import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.Arrays;
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
 /**
  * Created by Krasnal on 06.05.2017.
  */
 
-public class DisplayWyszukaj extends Activity {
-
-
-
+public class DisplayWyszukaj extends Activity
+{
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wyszukaj_layout);
-
     }
 }
