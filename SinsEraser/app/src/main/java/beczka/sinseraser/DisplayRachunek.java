0c32272d9854b2f418fca946b7d550665bdeaecc
MooVx <michal.tajak@gmail.com>
2017-05-06 14:17:55 +0200

    wszystkie trasy add

--- SinsEraser/app/src/main/java/beczka/sinseraser/DisplayRachunek.java
+++ SinsEraser/app/src/main/java/beczka/sinseraser/DisplayRachunek.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 
 import static beczka.sinseraser.R.layout.rachunek_layout;
 
-/**
- * Created by Krasnal on 06.05.2017.
- */
+
 
 public class DisplayRachunek extends Activity
 {
