74e55e1ee9fcefc7b43e4503599f1e7a7eb4228f
MooVx <michal.tajak@gmail.com>
2017-05-06 12:58:43 +0200

    links working

--- SinsEraser/app/src/main/java/beczka/sinseraser/MainActivity.java
+++ SinsEraser/app/src/main/java/beczka/sinseraser/MainActivity.java
@@ -1,7 +1,11 @@
 package beczka.sinseraser;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Browser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void web_beczka_open(View view) {
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.beczka.krakow.dominikanie.pl/"));
+        startActivity(browserIntent);
+    }
+
+    public void web_edk_open(View view) {
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.edk.org.pl/"));
+        startActivity(browserIntent);
+    }
+
+
+
 }
